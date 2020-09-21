1ffeeb280ce525a9775577efbd6ffac4a7d202d1
captainlintong <lintong784533@163.com>
2020-09-22 00:35:13 +0800

    画一个圆 有bug ，@vue/runtime-core里的h第三个参数传数组 就不显示 x undefined

--- code/play-plane/src/App.js
+++ code/play-plane/src/App.js
@@ -1,7 +1,11 @@
 import { defineComponent, h} from '@vue/runtime-core'
+import Circle from './component/Circle.js'
 export default defineComponent({
     render () {
-        const vnode = h('rect')
+        const vnode = h('rect', { x: 100, y: 100 }, [
+            '我是一个矩形',
+            h(Circle)
+        ])
         console.log(vnode)
         return vnode
     }
--- code/play-plane/src/runtime-canvas/index.js
+++ code/play-plane/src/runtime-canvas/index.js
@@ -1,5 +1,5 @@
 import { createRenderer } from '@vue/runtime-core'
-import { Graphics } from 'pixi.js'
+import { Graphics, Text } from 'pixi.js'
 const renderer = createRenderer({
     createElement(type) {
         console.log(type)
@@ -7,11 +7,30 @@ const renderer = createRenderer({
         if (type === 'rect') {
             // console.log('323232')
             element = new Graphics()
-            element.beginFill(0xFFFF00)
+            element.beginFill(0xFF0000)
             element.drawRect(0,0,500,500)
+            element.endFill()
+        } else if (type === 'circle ') {
+            console.log('323232')
+            element = new Graphics()
+            element.beginFill(0xffff00)
+            element.drawCircle(0,0,50)
             element.endFill()
         }
         return element
+    },
+
+    setElementText (node, text) {
+        const cText = new Text(text)
+        node.addChild(cText)
+    },
+    createText (text) {
+        return new Text(text)
+    },
+    patchProp (el, key, prevValue, nextValue) {
+        console.log(key)
+        console.log(el)
+        el[key] = nextValue
     },
     insert(el, parent) {
         console.log(el)
