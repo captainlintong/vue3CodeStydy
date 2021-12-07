bc0ca0c649c38192f03c8ac6ef64dc72f7e6b7b3
captainlintong <lintong784533@163.com>
2021-12-07 22:56:00 +0800

    vite + vue3 study

---
+++ code/vue3-vite01/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ msg }}
+    <div>{{ state.doubleCounter }}</div>
+    <div>{{ state.counter }}</div>
+  </div>
+</template>
+
+<script>
+import { computed, onMounted, onUnmounted, reactive, ref } from 'vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup() {
+    const state = reactive({
+      counter: 1,
+      doubleCounter: computed(() => state.counter *2)
+    })
+    const cunt = ref('d')
+    let timer
+    onMounted (() => {
+      setInterval(() => {
+        timer = state.counter++
+      }, 1000)
+    })
+    onUnmounted(() => {
+      clearInterval(timer)
+    })
+    return { state, cunt }
+  }
+}
+</script>
